2852c539984adc9c166367f40a298d74703d03a1
Maxence Gilliot <maxencegilliot@gmail.com>
2022-10-21 18:33:37 +0200

    [api] Add ESLint and Prettier

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,27 +1,27 @@
-import express from "express";
-import cors from "cors";
+import express from 'express'
+import cors from 'cors'
 
-const app = express();
+const app = express()
 
-var corsOptions = {
-  origin: "http://localhost:8081"
-};
+const corsOptions = {
+    origin: 'http://localhost:8081',
+}
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 // parse requests of content-type - application/json
-app.use(express.json());
+app.use(express.json())
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 // simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the Football Players application." });
-});
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to the Football Players application.' })
+})
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+    console.log(`Server is running on port ${PORT}.`)
+})
